ef084a0f1dad4e0e7d7d3c656cc13558bf01202f
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-01-30 13:43:06 +0530

    ui fix

--- src/components/popOverCart/popOverCart.js
+++ src/components/popOverCart/popOverCart.js
@@ -9,11 +9,11 @@ const PopOverCart = () => {
             <div className={styles.modal}>
                 <div className={styles.modalContent}>
                         {cartItems.map(product =>
-                            <div className={`row ${styles.poplist}`} key={product.id}>
-                                <div className=" col-2"> <span className="close " onClick={() => removeProduct(product)}>&times;</span> </div>
-                                <div className=" col-8"> 
-                                <h6  className={`${styles.title}`} >{product.title}</h6>
-                                <p className={`mb-1 ${styles.price}`}>${product.price}</p>   
+                            <div className={`row mx-0 ${styles.poplist}`} key={product.id}>
+                                <div className=" col-2"> <span className={`close ${styles.close} `} onClick={() => removeProduct(product)}>&times;</span> </div>
+                                <div className=" col-8 py-2"> 
+                                <h5  className={`${styles.title}`} >{product.title}</h5>
+                                <p className={` ${styles.price}`}>${product.price}</p>   
                                  </div>
                                  <div className=" col-2"> <span className="row">Qty{" "}{product.quantity}</span> </div>
                             </div>)}
