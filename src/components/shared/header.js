8b217c87e6a54ea7999ca89b6870a2486e246600
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-01-30 09:44:27 +0530

    all function done

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import { Link } from "react-router-dom";
 import styles from './header.module.scss';
 import { CartContext } from '../../contexts/CartContext';
-
+import PopOverCart from '../popOverCart/popOverCart';
 const Header = () => {
     const {itemCount,total} = useContext(CartContext);
+    const [showPopOver,setShowPopOver] = useState(false);
     return ( 
         <header className={`${styles.header} d-flex justify-content-center mx-5`}>
             <div className="d-flex justify-content-between w-100">
@@ -13,10 +14,11 @@ const Header = () => {
             <div className="d-flex">
             <Link to='/cart'><div className="d-flex flex-column "><span>${total}</span> <span>Items{" "}{itemCount}</span></div></Link>
             <span>
-                <img className={styles.icon} src="/images/cartB.png" alt="cart" />
+                <img onClick={() =>setShowPopOver(!showPopOver)}className={styles.icon} src="/images/cartB.png" alt="cart" />
             </span>
             </div>
             </div>
+            {showPopOver ? <PopOverCart /> : null}
         </header>
      );
 }
